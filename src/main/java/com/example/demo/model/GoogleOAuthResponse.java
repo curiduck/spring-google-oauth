d76a62a21c93fdb57af845e63c417dc3ff09fdf4
Gi Deok Kim <gdtbgl93@gmail.com>
2020-09-14 22:54:34 +0900

    demo

---
+++ src/main/java/com/example/demo/model/GoogleOAuthResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+
+@Data
+public class GoogleOAuthResponse {
+	
+	private String accessToken;
+	private String expiresIn;
+	private String refreshToken;
+	private String scope;
+	private String tokenType;
+	private String idToken;
+	
+	
+}
